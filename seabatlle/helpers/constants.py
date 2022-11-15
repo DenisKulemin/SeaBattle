a5b7b90cc59e367f6e7d72af711e50ad60a78cad
Denis Kuliomin <kuliomin.d.a@gmail.com>
2022-11-15 20:24:42 +0100

    Add unittest for game board

--- seabatlle/helpers/constants.py
+++ seabatlle/helpers/constants.py
@@ -1,11 +1,17 @@
 """Module with game constants."""
 
+EMPTY_SIGN = "empty"
+SHIP_SIGN = "ship"
+MISS_SIGN = "miss"
+HIT_SIGN = "hit"
+SIGN = "sign"
+VALUE = "value"
 
 GAME_OBJECTS = {
-    "empty": {"sign": " ", "value": 0},
-    "ship": {"sign": "O", "value": 1},
-    "miss": {"sign": "*", "value": 10},
-    "hit": {"sign": "X", "value": 1000}
+    EMPTY_SIGN: {SIGN: " ", VALUE: 0},
+    SHIP_SIGN: {SIGN: "O", VALUE: 1},
+    MISS_SIGN: {SIGN: "*", VALUE: 10},
+    HIT_SIGN: {SIGN: "X", VALUE: 1000}
 }
 
 AREA_AROUND = [(-1, -1), (-1, 0), (-1, 1), (0, -1), (0, 1), (1, -1), (1, 0), (1, 1)]
--- tests/unittests/game_board_test.py
+++ tests/unittests/game_board_test.py
@@ -1,1 +1,44 @@
 """Module with unittests for game board."""
+import pytest
+
+from seabatlle.game_errors.board_errors import BlockedAreaError, BlockedAreaAroundError, ShotCellEarlierError
+from seabatlle.game_objects.board import GameBoard
+from seabatlle.helpers.constants import GAME_OBJECTS, HIT_SIGN, MISS_SIGN, SIGN
+
+BOARD_GAME = GameBoard()
+BOARD_GAME.set_ship_coordinate([(1, 2), (2, 2)])
+BOARD_GAME.set_ship_coordinate([(6, 6)])
+BOARD_GAME.set_ship_coordinate([(9, 6), (9, 7), (9, 8)])
+
+
+def test_set_ship_coordinate():
+    """Method tests correct work of set_ship_coordinate method."""
+    board_game = GameBoard()
+    board_game.set_ship_coordinate([(1, 2), (2, 2)])
+    # Check if game board is not empty.
+    assert board_game != GameBoard()
+    # Check if method couldn't set a new ship on area with other ship's part.
+    with pytest.raises(BlockedAreaError):
+        board_game.set_ship_coordinate([(1, 2), (1, 3)])
+    # Check if method couldn't set a new ship in one cell perimeter around other ship.
+    with pytest.raises(BlockedAreaAroundError):
+        board_game.set_ship_coordinate([(2, 3), (3, 3)])
+    # Check if method raise TypeError if used wrong coordinate format.
+    with pytest.raises(TypeError):
+        board_game.set_ship_coordinate((3, 3))
+
+
+def test_shoot():
+    """Method tests correct work of shoot method."""
+    board_game = GameBoard()
+    board_game.set_ship_coordinate([(1, 2), (2, 2)])
+    # Check if sign after successful shooting is correct.
+    board_game.shoot((1, 2))
+    assert board_game.board[1][2] == GAME_OBJECTS.get(HIT_SIGN).get(SIGN)
+    # Check if sign after miss shooting is correct.
+    board_game.shoot((3, 3))
+    assert board_game.board[3][3] == GAME_OBJECTS.get(MISS_SIGN).get(SIGN)
+    # Check if method raise ShotCellEarlierError if cell was shot earlier.
+    with pytest.raises(ShotCellEarlierError):
+        board_game.shoot((1, 2))
+        board_game.shoot((3, 3))
