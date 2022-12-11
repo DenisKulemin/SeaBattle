9d7540c6a5de72746bc7d65eb007864960073757
Denis Kuliomin <kuliomin.d.a@gmail.com>
2022-12-11 11:48:20 +0100

    Update using existing cells in set ship method

--- seabattle/game.py
+++ seabattle/game.py
@@ -8,17 +8,17 @@ from seabattle.game_objects.battlefield import BattleField
 def main() -> None:
     """Method with main game logic."""
     command = input("Type your command (possible commands: set_ship, start_game, shoot, exit): ")
-    game_board = BattleField()
+    battlefield_area = BattleField()
     game_starts = False
 
-    game_board.set_ship_coordinate([(1, 2), (2, 2)])
-    game_board.set_ship_coordinate([(4, 4), (6, 6), (4, 6)])
+    battlefield_area.set_ship_coordinate([(1, 2), (2, 2)])
+    battlefield_area.set_ship_coordinate([(4, 4), (6, 6), (4, 6)])
 
     while command != "exit":
         if (command == "set_ship") and not game_starts:
             try:
                 coordinates = ast.literal_eval(input("Type coordinates in format [(x, y), (x, y)]: "))
-                game_board.set_ship_coordinate(coordinates)
+                battlefield_area.set_ship_coordinate(coordinates)
             except (BaseBattleFieldError, SyntaxError, TypeError) as exp:
                 print(exp)
         elif (command == "set_ship") and game_starts:
@@ -30,13 +30,13 @@ def main() -> None:
         elif (command == "shoot") and game_starts:
             try:
                 coordinate = ast.literal_eval(input("Type coordinate for shooting in format (x, y): "))
-                game_board.shoot(coordinate)
+                battlefield_area.shoot(coordinate)
             except (ShotCellEarlierError, SyntaxError, TypeError) as exp:
                 print(exp)
         else:
             print(f"Unknown command: {command}")
-        print(game_board)
-        if not game_board.game_is_over:
+        print(battlefield_area)
+        if not battlefield_area.game_is_over:
             command = input("Type your command (possible commands: set_ship, start_game, shoot, exit): ")
         else:
             command = "exit"
--- seabattle/game_objects/ship.py
+++ seabattle/game_objects/ship.py
@@ -1,22 +1,42 @@
 """Module for creation ship objects."""
 from dataclasses import dataclass
-from typing import List, Tuple
 
 from seabattle.game_errors.ship_errors import NotParallelShipError, WrongShipSizeError, WrongShipCoordinateError
 from seabattle.game_objects.cell import Cell
+from seabattle.helpers.constants import SignObjects
 
 
 @dataclass
 class Ship:
     """Class contains ship object and its methods."""
 
-    def __init__(self, coordinates: List[Tuple[int, int]]):
-        self._check_coordinates(coordinates)
+    def __init__(self, ship_cells: dict[tuple, Cell]):
+        self._check_coordinates(list(ship_cells.keys()))
 
-        self.ship = {(x, y): Cell(x=x, y=y) for x, y in coordinates}
+        self.ship = self._set_ship_sign(ship_cells)
 
     @staticmethod
-    def _check_coordinates(coordinates):
+    def _set_ship_sign(ship_cells: dict[tuple, Cell]) -> dict[tuple, Cell]:
+        """
+        Method changes cell signs from empty to ship sign.
+        Args:
+            ship_cells: Dictionary with cell coordinates as keys, and cells as values
+
+        Returns:
+            Dictionary with cell coordinates and cells with updated sings.
+        """
+        for cell in ship_cells.values():
+            cell.sign = SignObjects.ship_sign.sign
+
+        return ship_cells
+
+    @staticmethod
+    def _check_coordinates(coordinates: list[tuple]):
+        """
+        Method checks giving coordinates if they acceptable as ship coordinates.
+        Args:
+            coordinates: List of tuples with coordinates.
+        """
         ship_len = len(coordinates)
         x_size = abs(coordinates[0][0] - coordinates[-1][0])
         y_size = abs(coordinates[0][1] - coordinates[-1][1])
